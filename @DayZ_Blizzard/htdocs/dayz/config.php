33d65c6fe13d608d9d16775b4a0e2929c50fc807
Crosire <crosiregaming@googlemail.com>
2012-08-23 00:16:02 +0200

    Basic Webpages upload
    Uploading Chive and DayZ Admin files

---
+++ @DayZ_Blizzard/htdocs/dayz/config.php
@@ -0,0 +1,22 @@
+<?php
+	defined('DS') ? null : define('DS',DIRECTORY_SEPARATOR);
+	$path = ""; 				//path to the files, ex. "/admin/"
+
+	$serverip = "127.0.0.1";  		//server host
+	$serverport = 2302; 			//server port
+	$rconpassword = "adminpass"; 		//rcon password
+
+	$hostname = "127.0.0.1";  		//database host
+	$username = "dayz"; 			//database user
+	$password = "123456"; 			//database password
+	$dbName = "dayz"; 			//database name
+
+	$sitename = "DayZ Administration"; 	//Admin panel name
+
+	$gamepath = "";				//path to the game
+	$gameexe = "arma2oaserver.exe"; 	//server executable name
+	$exepath = ""; 				//executable path
+
+	$serverstring = " -beta=Expansion".DS."beta;Expansion".DS."beta".DS."Expansion -arma2netdev -mod=@Arma2NET;@CBA;@CBA_A2;@CBA_OA;@DayZ;@DAyZ_Blizzard; -name=Server -config=config.cfg -cfg=basic.cfg -profiles=Server -cpuCount=2 -maxMem=1578"; //server startup parametres
+	$config = "config.cfg"; 		//server config file
+?>
