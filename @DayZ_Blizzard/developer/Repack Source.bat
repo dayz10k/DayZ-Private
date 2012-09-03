2f6b3c29f6a35d186884697a1bc78b363eecfe28
Crosire <crosiregaming@googlemail.com>
2012-09-03 09:55:42 +0200

    Fixed developer repack script

--- @DayZ_Blizzard/developer/Repack Source.bat
+++ @DayZ_Blizzard/developer/Repack Source.bat
@@ -3,6 +3,6 @@ mkdir ..\addons
 mkdir ..\..\MPMissions
 Utils\cpbo.exe -y -p Source\dayz_server ..\addons\dayz_server.pbo
 Utils\cpbo.exe -y -p Source\dayz_lingor ..\addons\dayz_server_lingor.bak
-Utils\cpbo.exe -y -p Source\missions\dayz.lingor ..\..\MPMissions\dayz.lingor.pbo
-Utils\cpbo.exe -y -p Source\missions\dayz.chernarus ..\..\MPMissions\dayz.chernarus.pbo
+Utils\cpbo.exe -y -p Source\missions\dayz_1.lingor ..\..\MPMissions\dayz_1.lingor.pbo
+Utils\cpbo.exe -y -p Source\missions\dayz_1.chernarus ..\..\MPMissions\dayz_1.chernarus.pbo
 pause
