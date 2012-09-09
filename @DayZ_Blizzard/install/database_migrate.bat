4e4161a8f013716c34645545692e22cdf6f2c9cf
Crosire <crosiregaming@googlemail.com>
2012-09-09 11:43:29 +0200

    Improved migration batch with root password argument

--- @DayZ_Blizzard/install/database_migrate.bat
+++ @DayZ_Blizzard/install/database_migrate.bat
@@ -1,4 +1,10 @@
 @echo off
 cd ..\developer
-..\perl\bin\perl -w Schema\db_migrate.pl -database %1
-..\perl\bin\perl -w Schema\db_migrate.pl -database %2
+
+if "%3"=="" (
+..\perl\bin\perl -w Schema\db_migrate.pl -database %1 -username root
+..\perl\bin\perl -w Schema\db_migrate.pl -database %2 -username root
+) else (
+..\perl\bin\perl -w Schema\db_migrate.pl -database %1 -username root -password %3
+..\perl\bin\perl -w Schema\db_migrate.pl -database %2 -username root -password %3
+)
