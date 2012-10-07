0187a29a49fb2a05453afc6ebb0db0bd946da0e4
Crosire <crosiregaming@googlemail.com>
2012-10-07 23:10:33 +0200

    Fixed missing table

--- @DayZ_Blizzard/install/schema/Blizzard/mysql/0.01/00_privileges.sql
+++ @DayZ_Blizzard/install/schema/Blizzard/mysql/0.01/00_privileges.sql
@@ -2,3 +2,4 @@
 grant all on dayz_chernarus.* to 'dayz'@'localhost' identified by 'dayz';
 grant all on dayz_lingor.* to 'dayz'@'localhost' identified by 'dayz';
 grant all privileges on mysql.proc to 'dayz'@'localhost';
+grant all privileges on mysql.user to 'dayz'@'localhost';
