f05aff50db25ebc261152677d2f4cc145c2056ee
Crosire <crosiregaming@googlemail.com>
2012-10-26 20:36:17 +0200

    Crashsite map now only shows latest locations

--- @DayZCC/htdocs/dayz/modules/maps/6.php
+++ @DayZCC/htdocs/dayz/modules/maps/6.php
@@ -1,5 +1,5 @@
 <?
-	// Thanks to user WarZ for his code!
+	// Thanks to user WarZ for parts of his code and TorZar for giving me ideas on howto solve a problem!
 	error_reporting(E_ALL ^ E_NOTICE);
 
 	if (file_exists($path_logrpt))
@@ -7,18 +7,30 @@
 		$lines = file($path_logrpt);
 		$markers = "var markers = [";
 		$k = 0;
+		$i = 0;
 		
+		$s = 0;
+		foreach ($lines as $line):
+			if (strpos($line,"SERVER: INITIALIZED!") !== false) {$i = $s;}
+			$s++;
+		endforeach;
+
+		$s = 0;
 		foreach ($lines as $line):
 			$matches = array();
 			$sectionpattern = "/((?<=[a-z]{10}\s[a-z]{2}\s\[)[0-9]{0,20}+\.+[0-9]{0,10}+\,+[0-9]{0,20}+\.+[0-9]{0,10})/x";
-			if (preg_match($sectionpattern, $line, $matches)):
-				$currentsection = $matches[1];
-				$Worldspace = explode(",", $currentsection);
+			
+			if ($s > $i)
+			{
+				if (preg_match($sectionpattern, $line, $matches)):
+					$Worldspace = explode(",", $matches[1]);
 
-				include_once($path.'modules/calc.php');
-				$description = "<h2>Heli crash site</h2><table><tr><td><img style=\"max-width: 100px;\" src=\"".$path."images/vehicles/Crashsite.png\"></td><td>&nbsp;</td><td style=\"vertical-align:top; \"><h2>Position:</h2>".world_pos2_crash($Worldspace, $serverworld)."</td></tr></table>";
-				$markers .= "['Crash site', '".$description."', ".world_pos_y_crash($Worldspace, $serverworld). ", ".world_pos_x_crash($Worldspace, $serverworld).", ".$k++.", '".$path."images/icons/Crashsite.png'],";
-			endif;
+					include_once($path.'modules/calc.php');
+					$description = "<h2>Heli crash site</h2><table><tr><td><img style=\"max-width: 100px;\" src=\"".$path."images/vehicles/Crashsite.png\"></td><td>&nbsp;</td><td style=\"vertical-align:top; \"><h2>Position:</h2>".world_pos2_crash($Worldspace, $serverworld)."</td></tr></table>";
+					$markers .= "['Crash site', '".$description."', ".world_pos_y_crash($Worldspace, $serverworld). ", ".world_pos_x_crash($Worldspace, $serverworld).", ".$k++.", '".$path."images/icons/Crashsite.png'],";
+				endif;
+			}
+			$s++;
 		endforeach;
 
 		$markers .= "['Edge of map', 'Edge of map', 0.0, 0.0, 1, '" . $path . "images/thumbs/null.png']];";
