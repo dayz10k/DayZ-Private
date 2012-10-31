14c69f128c7b50d409adae0ee55d127be0d16cf9
Crosire <crosiregaming@googlemail.com>
2012-10-31 04:44:30 +0100

    Updated whitelist schema to include the player name

--- @DayZCC/install/schema/DayZCC/mysql/0.03-0.04/10_whitelist.sql
+++ @DayZCC/install/schema/DayZCC/mysql/0.03-0.04/10_whitelist.sql
@@ -1,2 +1,8 @@
-alter table profile
-  add column guid varchar(32) not null after unique_id;
+create table if not exists whitelist (
+	`id`				int(11) unsigned not null auto_increment,
+	`name`				varchar(64) not null default '',
+	`guid`				varchar(32) not null default '',
+	`is_whitelisted`	tinyint(1) default 0,
+
+	primary key (`id`)
+) character set utf8 engine=InnoDB;
