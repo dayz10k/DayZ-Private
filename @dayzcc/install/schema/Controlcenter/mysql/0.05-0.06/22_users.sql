7b49aceca88d208c624b503791ff927ba7e34fd0
Crosire <crosiregaming@googlemail.com>
2012-11-15 00:14:21 +0100

    Fixed migration not updating web admin permissions correctly

--- @dayzcc/install/schema/Controlcenter/mysql/0.05-0.06/22_users.sql
+++ @dayzcc/install/schema/Controlcenter/mysql/0.05-0.06/22_users.sql
@@ -1,1 +1,5 @@
-update users set permissions = replace(permissions,',list',',table');
+update users set permissions = replace(permissions, 'list', 'table');
+update users set permissions = replace(permissions, 'whitetable', 'whitelist');
+
+alter table users
+  modify permissions varchar(50) not null default 'whitelist, table, map, control';
