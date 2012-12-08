9f48aec52ca2504bbd17558ac9d9be2f46a5579d
Crosire <crosiregaming@googlemail.com>
2012-12-08 15:12:12 +0100

    Moved "BlissBuildings" and "BlissInvCust" migration into migrate.pl, no need to execute it manually anymore

--- @dayzcc/install/migrate.pl
+++ @dayzcc/install/migrate.pl
@@ -1,5 +1,5 @@
 #!/usr/bin/perl -w
-# Bliss and Controlcenter schema migration utility 
+# Database schema migration utility 
 # by ayan4m1 and Crosire
 
 use Getopt::Long;
@@ -16,8 +16,6 @@ GetOptions(
 	'password|pass|dbpass|p=s',
 	'database|name|dbname|d=s',
 	'port|dbport=s',
-	'schema|s=s',
-	'version|v=s',
 	'help'
 );
 
@@ -34,30 +32,36 @@ if ($args{'help'}) {
 	exit;
 }
 
-print "Starting up ...";
+print "Starting up ...\n";
 die "> Error: Schema version must be specified for a non-standard schema!\n" if ($args{'schema'} && !defined $args{'version'});
+print "\n";
 
-my $schema1 = $args{'schema'} ? $args{'schema'} : "Bliss";
+my $schema1 = "Bliss";
 my $schema2 = "Controlcenter";
-my $version1 = $args{'version'} ? $args{'version'} : "0.35";
+my $version1 = "0.35";
 my $version2 = "0.07";
 
 print "Trying to connect to MySQL server on $db{'host'}, database $db{'name'} as $db{'user'}\n";
 my $dbh = DBIx::Transaction->connect("dbi:mysql:$db{'name'}:$db{'host'}:$db{'port'}", $db{'user'}, $db{'pass'}) or die "> Error: MySQL Error: ".DBI->errstr."\n";
 print "> Sucessfully connected!\n\n";
 
-my $m1 = DBIx::Migration::Directories->new(base => dirname(__FILE__) . '/schema', schema => $schema1, desired_version => $version1, dbh => $dbh);
-my $m2 = DBIx::Migration::Directories->new(base => dirname(__FILE__) . '/schema', schema => $schema2, desired_version => $version2, dbh => $dbh);
-
-if ($m1->get_current_version()) {printf("Current Bliss schema version is %.2f\n", $m1->get_current_version());} else {print "No existing Bliss schema was found!\n";}
-if ($m2->get_current_version()) {printf("Current Controlcenter schema version is %.2f\n", $m2->get_current_version());} else {print "No existing Controlcenter schema was found!\n";}
+my $m1 = DBIx::Migration::Directories->new(base => dirname(__FILE__).'/schema', schema => $schema1, desired_version => $version1, dbh => $dbh);
+my $m2 = DBIx::Migration::Directories->new(base => dirname(__FILE__).'/schema', schema => $schema2, desired_version => $version2, dbh => $dbh);
+if ($m1->get_current_version()) { printf("Current ".$schema1." schema version is %.2f\n", $m1->get_current_version()); } else { print "No existing ".$schema1." schema was found!\n"; }
+if ($m2->get_current_version()) { printf("Current ".$schema2." schema version is %.2f\n", $m2->get_current_version()); } else { print "No existing ".$schema2." schema was found!\n"; }
 print "\n";
 
 print "Running database migration ...\n\n";
 $m1->migrate or die "> Error: Migration failed!\n";
 $m2->migrate or die "> Error: Migration failed!\n";
 print "\n";
-
-print "> Completed Bliss update to version $version1\n";
-print "> Completed Controlcenter update to version $version2\n";
+print "> Completed ".$schema1." update to version $version1\n";
+print "> Completed ".$schema2." update to version $version2\n";
 print "\n";
+
+print "Running optional package migration ...\n\n";
+$m1 = DBIx::Migration::Directories->new(base => dirname(__FILE__).'/schema', schema => "BlissBuildings", desired_version => "0.01", dbh => $dbh);
+$m1->migrate;
+$m1 = DBIx::Migration::Directories->new(base => dirname(__FILE__).'/schema', schema => "BlissInvCust", desired_version => "0.02", dbh => $dbh);
+$m1->migrate;
+print "\n> Completed optional package update";
