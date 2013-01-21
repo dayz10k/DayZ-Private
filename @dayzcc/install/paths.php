728d3a562af89003b416bf5d7cc86ad3cd3b648e
Crosire <crosiregaming@googlemail.com>
2013-01-21 19:34:47 +0100

    Rewrote file configuration script

---
+++ @dayzcc/install/paths.php
@@ -0,0 +1,24 @@
+<?php
+	$backslashrootreal[] = "$partwampp\php\\php.ini";
+	$backslashrootreal[] = "$partwampp\apache\bin\\apr-config.bat";
+	$backslashrootreal[] = "$partwampp\apache\bin\\apr-config.pl";
+	$backslashrootreal[] = "$partwampp\apache\bin\\apu-config.bat";
+	$backslashrootreal[] = "$partwampp\apache\bin\\apu-config.pl";
+	$backslashrootreal[] = "$partwampp\apache\bin\\apr-1-config.bat";
+	$backslashrootreal[] = "$partwampp\apache\bin\\apu-1-config.bat";
+	$backslashrootreal[] = "$partwampp\apache\bin\\apxs.bat";
+	$backslashrootreal[] = "$partwampp\apache\bin\\apxs.pl";
+	$slashrootreal[] =  "$partwampp\mysql\bin\\my.ini";
+	$slashrootreal[] = "$partwampp\apache\conf\\httpd.conf";
+	$slashrootreal[] = "$partwampp\apache\conf\\httpd-autoindex.conf";
+	$slashrootreal[] = "$partwampp\apache\conf\\httpd-dav.conf";
+	$slashrootreal[] = "$partwampp\apache\conf\\httpd-default.conf";
+	$slashrootreal[] = "$partwampp\apache\conf\\httpd-info.conf";
+	$slashrootreal[] = "$partwampp\apache\conf\\httpd-languages.conf";
+	$slashrootreal[] = "$partwampp\apache\conf\\httpd-manual.conf";
+	$slashrootreal[] = "$partwampp\apache\conf\\httpd-mpm.conf";
+	$slashrootreal[] = "$partwampp\apache\conf\\httpd-ssl.conf";
+	$slashrootreal[] = "$partwampp\apache\conf\\httpd-userdir.conf";
+	$slashrootreal[] = "$partwampp\apache\conf\\httpd-vhosts.conf";
+	$slashrootreal[] = "$partwampp\apache\conf\\httpd-more.conf";
+?>
