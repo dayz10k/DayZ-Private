1ef77f0e7e6bc79f14864002de92b4aee38ee163
Crosire <crosiregaming@googlemail.com>
2013-01-23 21:21:40 +0100

    Added possibility to edit web admin users

--- @dayzcc/htdocs/dayz/modules/register.php
+++ @dayzcc/htdocs/dayz/modules/register.php
@@ -1,125 +1,71 @@
 <?php
 
 if (isset($_SESSION['user_id']) and (strpos($_SESSION['user_permissions'], "user") !== false))
-{ 
-	function GenerateSalt($n = 3) { 
-		$key = '';
-		$pattern = '1234567890abcdefghijklmnopqrstuvwxyz.,*_-=+';
-		$counter = strlen($pattern)-1;
-		for ($i = 0; $i < $n; $i++) { $key .= $pattern{rand(0, $counter)}; }
-		return $key;
+{
+	$login = "Username";
+	$password = "Password";
+	$checked = array('manage' => true, 'control' => true, 'table' => true, 'map' => true, 'tools' => true, 'feed' => true, 'user' => true, 'whitelist' => true);
+	
+	if (isset($_GET['edit'])) {
+		$id = $_GET['edit'];
+		$login = mysql_fetch_assoc(mysql_query("SELECT `login` FROM `users` WHERE `id` = '{$id}' LIMIT 1"))['login'];
+		$checked = array('manage' => false, 'control' => false, 'table' => false, 'map' => false, 'tools' => false, 'feed' => false, 'user' => false, 'whitelist' => false);
+		
+		foreach (explode(',', str_replace(" ", "", mysql_fetch_assoc(mysql_query("SELECT `permissions` FROM `users` WHERE `id` = '{$id}' LIMIT 1"))['permissions'])) as $check) {
+			$checked[$check] = true;
+		}
 	}
 
-	if (empty($_POST))
-	{
-		?>
+	?>
 		
-		<div id="page-heading">
-			<h1>Registration</h1>
-		</div>
+	<div id="page-heading">
+		<h1>Registration</h1>
+	</div>
 
-		<table id="content-table" border="0" width="100%" cellpadding="0" cellspacing="0">
-			<tr>
-				<th rowspan="3"><img src="images/forms/side_shadowleft.jpg" width="20" height="300" alt="" /></th>
-				<th class="corner-topleft"></th>
-				<td class="border-top">&nbsp;</td>
-				<th class="corner-topright"></th>
-				<th rowspan="3"><img src="images/forms/side_shadowright.jpg" width="20" height="300" alt="" /></th>
-			</tr>
-			<tr>
-				<td class="border-left"></td>
-				<td>
-					<div id="content-table-inner">
-						<div id="table-content">
-							<form action="index.php?view=register" method="post">
-								<h2>Enter login name and password for the new user:</h2>
-								<input type="text" name="login" value="Username" onblur="if (this.value == '') { this.value = 'Username'; }" onfocus="if (this.value == 'Username') { this.value = ''; }" style="display: inline; padding-top: 6px; padding-bottom: 6px; padding-left: 6px; width: 200px;" />
-								<input type="text" name="password" value="Password" onblur="if (this.value == '') { this.value = 'Password'; }" onfocus="if (this.value == 'Password') { this.value = ''; }" style="display: inline; padding-top: 6px; padding-bottom: 6px; padding-left: 6px; width: 200px;" />
-								<br /><br />
-								<h2>Select the pages the user should be allowed to view:</h2>
-								<div style="border: 2px solid #ccc; width: 403px; height: 100px; padding-top: 6px; padding-left: 6px; overflow-y: scroll;">
-									<input type="hidden" name="permission" />
-									<input type="checkbox" name="manage" checked />&nbsp;&nbsp;&nbsp;"Manage Overview"<br />
-									<input type="checkbox" name="control" checked />&nbsp;&nbsp;&nbsp;"Server Control", "Logs", "BattlEye", "Bans"<br />
-									<input type="checkbox" name="table" checked />&nbsp;&nbsp;&nbsp;"Playerlist", "Vehiclelist", "Deployablelist", "Check items", "Search"<br />
-									<input type="checkbox" name="map" checked />&nbsp;&nbsp;&nbsp;"Playermap", "Vehiclemap", "Deployablemap", "Wreckmap"<br />
-									<input type="checkbox" name="tools" checked />&nbsp;&nbsp;&nbsp;"Vehicle import tools"<br />
-									<input type="checkbox" name="feed" checked />&nbsp;&nbsp;&nbsp;"Killfeed"<br />
-									<input type="checkbox" name="user" checked />&nbsp;&nbsp;&nbsp;"Accounts"<br />
-								</div>
-								<br />
-								<input type="submit" class="submit" />
-							</form>
-						</div>
-						<div class="clear"></div>
+	<table id="content-table" border="0" width="100%" cellpadding="0" cellspacing="0">
+		<tr>
+			<th rowspan="3"><img src="images/forms/side_shadowleft.jpg" width="20" height="300" alt="" /></th>
+			<th class="corner-topleft"></th>
+			<td class="border-top">&nbsp;</td>
+			<th class="corner-topright"></th>
+			<th rowspan="3"><img src="images/forms/side_shadowright.jpg" width="20" height="300" alt="" /></th>
+		</tr>
+		<tr>
+			<td class="border-left"></td>
+			<td>
+				<div id="content-table-inner">
+					<div id="table-content">
+						<form action="index.php?view=users<?php echo (isset($_GET['edit']) ? '&edit='.$id : ''); ?>" method="post">
+							<h2>Enter login name and password for the user:</h2>
+							<input type="text" name="login" value="<?php echo $login; ?>" <?php echo ($type == "edit" ? 'readonly' : ''); ?> onblur="if (this.value == '') { this.value = 'Username'; }" onfocus="if (this.value == 'Username') { this.value = ''; }" style="display: inline; padding-top: 6px; padding-bottom: 6px; padding-left: 6px; width: 200px;" />
+							<input type="text" name="password" value="<?php echo $password; ?>" onblur="if (this.value == '') { this.value = 'Password'; }" onfocus="if (this.value == 'Password') { this.value = ''; }" style="display: inline; padding-top: 6px; padding-bottom: 6px; padding-left: 6px; width: 200px;" />
+							<br /><br />
+							<h2>Select the pages the user should be allowed to view:</h2>
+							<div style="border: 2px solid #ccc; width: 403px; height: 100px; padding-top: 6px; padding-left: 6px; overflow-y: scroll;">
+								<?php
+									$permissions = array( array('manage', '"Manage Overview"'), array('control', '"Server Control", "Logs", "BattlEye", "Bans"'), array('table', '"Playerlist", "Vehiclelist", "Deployablelist", "Check items", "Search"'), array('map', '"Playermap", "Vehiclemap", "Deployablemap", "Wreckmap"'), array('tools', '"Vehicle import tools"'), array('feed', '"Killfeed"'), array('whitelist', '"Whitelist"'), array('user', '"Accounts"') );
+									for ($i = 0; $i < count($permissions); $i++) {
+										echo '<input type="checkbox" name="'.$permissions[$i][0].'" '.($checked[$permissions[$i][0]] ? 'checked' : '').' />&nbsp;&nbsp;&nbsp;'.$permissions[$i][1].'<br />';
+									}
+								?>
+							</div>
+							<br />
+							<input type="submit" class="submit" />
+						</form>
 					</div>
-				</td>
-				<td class="border-right"></td>
-			</tr>
-			<tr>
-				<th class="corner-bottomleft"></th>
-				<td class="border-bottom">&nbsp;</td>
-				<th class="corner-bottomright"></th>
-			</tr>
-		</table>
+					<div class="clear"></div>
+				</div>
+			</td>
+			<td class="border-right"></td>
+		</tr>
+		<tr>
+			<th class="corner-bottomleft"></th>
+			<td class="border-bottom">&nbsp;</td>
+			<th class="corner-bottomright"></th>
+		</tr>
+	</table>
 
-	<?php
-	}
-	else
-	{ 
-		$login = (isset($_POST['login'])) ? mysql_real_escape_string($_POST['login']) : '';
-		$password = (isset($_POST['password'])) ? mysql_real_escape_string($_POST['password']) : '';
-		$permissions = '';
-		$error = false;
-		$errort = '';
-		
-		if (strlen($login) < 2) { 
-			$error = true;
-			$errort .= 'Login must be at least 2 characters. ';
-		}
-		if (strlen($password) < 6) { 
-			$error = true;
-			$errort .= 'Password must be at least 6 characters. ';
-		}
-		if (mysql_num_rows(mysql_query("SELECT `id` FROM `users` WHERE `login` = '{$login}' LIMIT 1")) == 1) { 
-			$error = true;
-			$errort .= 'Login already used. ';
-		}
-
-		if (isset($_POST['permission'])) {
-			foreach (array('manage', 'control', 'table', 'map', 'tools', 'feed', 'user') as $permission) {
-				if (isset($_POST[$permission])) {
-					if ($_POST[$permission] == "on") { $permissions .= $permission.", "; }
-				}
-			}
-			$permissions = mysql_real_escape_string(substr($permissions, 0, -2));
-		}
-
-		if (!$error)
-		{ 
-			$salt = GenerateSalt();
-			$hash = md5(md5($password).$salt);
-			
-			mysql_query("INSERT INTO `users` SET `login` = '{$login}', `password` = '{$hash}', `salt` = '{$salt}', `permissions` = '{$permissions}'") or die(mysql_error());
-			mysql_query("INSERT INTO `log_tool` (`action`, `user`, `timestamp`) VALUES ('REGISTERED USER: {$login}', '{$_SESSION['login']}', NOW())");
-			
-			$delresult = '<div id="message-green">
-				<table border="0" width="100%" cellpadding="0" cellspacing="0"><tr>
-				<td class="green-left">User "'.$login.'" succesfully registered!</td>
-				<td class="green-right"><a class="close-green"><img src="images/forms/icon_close_green.gif" alt="" /></a></td>
-				</tr></table></div>';
-			include('modules/users.php');
-		}
-		else
-		{
-			$delresult = '<div id="message-red">
-				<table border="0" width="100%" cellpadding="0" cellspacing="0"><tr>
-				<td class="red-left">Error: '.$errort.'</td>
-				<td class="red-right"><a class="close-red"><img src="images/forms/icon_close_red.gif" alt="" /></a></td>
-				</tr></table></div>';
-			include('modules/users.php');
-		}
-	}
+<?php
 }
 else
 { 
--- @dayzcc/htdocs/dayz/modules/users.php
+++ @dayzcc/htdocs/dayz/modules/users.php
@@ -3,23 +3,106 @@
 if (isset($_SESSION['user_id']) and (strpos($_SESSION['user_permissions'], "user") !== false))
 { 
 	$pagetitle = "Manage users";
-	if (!isset($delresult)) { $delresult = ''; }
+	$delresult = '';
 	
-	if (isset($_POST["user"]))
-	{ 
+	function GenerateSalt($n = 3) { 
+		$key = '';
+		$pattern = '1234567890abcdefghijklmnopqrstuvwxyz.,*_-=+';
+		$counter = strlen($pattern)-1;
+		for ($i = 0; $i < $n; $i++) { $key .= $pattern{rand(0, $counter)}; }
+		return $key;
+	}
+	
+	if (isset($_POST['login']) && isset($_POST['password']))
+	{
+		if (isset($_GET['edit'])) {
+			$type = "edit";
+			$id = $_GET['edit'];
+		} else {
+			$type = "register";
+		}
+		
+		$login = mysql_real_escape_string($_POST['login']);
+		$password = mysql_real_escape_string($_POST['password']);
+		$permissions = '';
+		$error = false;
+		$errortext = '';
+		
+		if (strlen($login) < 2) {
+			$error = true;
+			$errortext .= 'Login must be at least 2 characters. ';
+		}
+		if (strlen($password) < 6) {
+			$error = true;
+			$errortext .= 'Password must be at least 6 characters. ';
+		}
+		if ($password == "Password") {
+			$error = true;
+			$errortext .= 'Invalid password, use another one. ';
+		}
+		
+		if (mysql_num_rows(mysql_query("SELECT `id` FROM `users` WHERE `login` = '{$login}' LIMIT 1")) == 1 && $type != "edit") {
+			$error = true;
+			$errortext .= 'Login already used. ';
+		}
+
+		foreach (array('manage', 'control', 'table', 'map', 'tools', 'feed', 'user', 'whitelist') as $permission) {
+			if (isset($_POST[$permission])) {
+				if ($_POST[$permission] == "on") { $permissions .= $permission.", "; }
+			}
+		}
+		$permissions = mysql_real_escape_string(substr($permissions, 0, -2));
+
+		if (!$error) {
+			$salt = GenerateSalt();
+			$hash = md5(md5($password).$salt);
+			
+			if ($type == "edit") {
+				mysql_query("UPDATE `users` SET `login` = '{$login}', `password` = '{$hash}', `salt` = '{$salt}', `permissions` = '{$permissions}' WHERE `id` = '{$id}'") or die(mysql_error());
+				mysql_query("INSERT INTO `log_tool` (`action`, `user`, `timestamp`) VALUES ('EDITED USER: {$login}', '{$_SESSION['login']}', NOW())");
+			} else {
+				mysql_query("INSERT INTO `users` SET `login` = '{$login}', `password` = '{$hash}', `salt` = '{$salt}', `permissions` = '{$permissions}'") or die(mysql_error());
+				mysql_query("INSERT INTO `log_tool` (`action`, `user`, `timestamp`) VALUES ('REGISTERED USER: {$login}', '{$_SESSION['login']}', NOW())");
+			}
+
+			$delresult = '<div id="message-green">
+				<table border="0" width="100%" cellpadding="0" cellspacing="0"><tr>
+				<td class="green-left">User "'.$login.'" was '.($type == "edit" ? 'edited' : 'added').'!</td>
+				<td class="green-right"><a class="close-green"><img src="images/forms/icon_close_green.gif" alt="" /></a></td>
+				</tr></table></div>';
+		} else {
+			$delresult = '<div id="message-red">
+				<table border="0" width="100%" cellpadding="0" cellspacing="0"><tr>
+				<td class="red-left">Error: '.$errortext.'</td>
+				<td class="red-right"><a class="close-red"><img src="images/forms/icon_close_red.gif" alt="" /></a></td>
+				</tr></table></div>';
+		}
+	}
+	else if (isset($_POST["user"]))
+	{
 		$deluser = $_POST["user"];
 		$delresult = '<div id="message-green"><table border="0" width="100%" cellpadding="0" cellspacing="0"><tr><td class="green-left">User ';
 
-		for ($i = 0; $i < count($deluser); $i++) { 
+		for ($i = 0; $i < count($deluser); $i++) {
 			mysql_query("INSERT INTO `log_tool` (`action`, `user`, `timestamp`) VALUES ('DELETED USER: ".$deluser[$i]."', '{$_SESSION['login']}', NOW())");
 			mysql_query("DELETE FROM `users` WHERE `id` = '".$deluser[$i]."'") or die(mysql_error());
 			
 			$delresult .= $deluser[$i].", ";
 		}
 		
-		$delresult = substr($delresult, 0, -2).' successfully removed!</td><td class="green-right"><a class="close-green"><img src="images/forms/icon_close_green.gif" alt="" /></a></td></tr></table></div>';
+		$delresult = substr($delresult, 0, -2).' removed!</td><td class="green-right"><a class="close-green"><img src="images/forms/icon_close_green.gif" alt="" /></a></td></tr></table></div>';
 	}
 	else if (isset($_GET['register']))
+	{
+		?>
+			<div id="Popup" class="modalPopup" style="display: none;">
+				<a id="closebutton" style="float: right;" href="#" onclick="HideModalPopup('Popup'); return false;"><img src="images/forms/action_delete.gif" alt="" /></a><br />
+				<?php include_once('modules/register.php'); ?>
+			</div>
+			<script type="text/javascript">ShowModalPopup('Popup');</script>
+		<?php
+	}
+	else if (isset($_GET['edit']))
 	{
 		?>
 			<div id="Popup" class="modalPopup" style="display: none;">
@@ -37,9 +120,11 @@ if (isset($_SESSION['user_id']) and (strpos($_SESSION['user_permissions'], "user
 	$res = mysql_query("SELECT * FROM `users` ORDER BY `id` ASC") or die(mysql_error());
 	$number = mysql_num_rows($res);
 	$users = '';
+
 	while ($row = mysql_fetch_array($res)) {
 		$users .= '<tr>
 			<td align="center" style="height: 47px;"><input name="user[]" value="'.$row['id'].'" type="checkbox"/></td>
+			<td align="center" style="height: 47px;"><a href="index.php?view=users&edit='.$row['id'].'"><img src="images/forms/action_edit.gif" title="Edit" alt="Edit" /></a></td>
 			<td align="center" style="height: 47px;">'.$row['id'].'</td>
 			<td align="center" style="height: 47px;">'.$row['login'].'</td>
 			<td align="center" style="height: 47px;">'.$row['permissions'].'</td>
@@ -82,7 +167,8 @@ if (isset($_SESSION['user_id']) and (strpos($_SESSION['user_permissions'], "user
 						<form action="index.php?view=users" method="post">
 							<table id="product-table" border="1" width="100%" cellpadding="0" cellspacing="0">
 								<tr>
-									<th class="product-table-header" style="width: 10%"><a>Delete</a></th>
+									<th class="product-table-header" style="width: 5%"><a>Delete</a></th>
+									<th class="product-table-header" style="width: 5%"><a>Edit</a></th>
 									<th class="product-table-header" style="width: 5%"><a>ID</a></th>
 									<th class="product-table-header" style="width: 25%"><a>Username</a></th>
 									<th class="product-table-header" style="width: 40%"><a>Permissions</a></th>
