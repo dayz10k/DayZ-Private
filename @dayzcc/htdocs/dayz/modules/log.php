2e31292ee4d3488ce49f7989154d0dc086fc262a
Crosire <crosiregaming@googlemail.com>
2013-02-09 11:54:31 +0100

    Fixed security exploit making it possible to download every file on the server

--- @dayzcc/htdocs/dayz/modules/log.php
+++ @dayzcc/htdocs/dayz/modules/log.php
@@ -2,10 +2,13 @@
 
 if (isset($_SESSION['user_id']) and (strpos($_SESSION['user_permissions'], "manage") !== false))
 {
-	$pagetitle = "Logs";
+	$pagetitle = "Server Logs";
+	$type = "server";
 
 	if (isset($_GET['type']))
 	{
+		$type = $_GET['type'];
+		
 		switch($_GET['type'])
 		{
 			case "battleye":
@@ -38,62 +41,62 @@ if (isset($_SESSION['user_id']) and (strpos($_SESSION['user_permissions'], "mana
 			<div id="content-table-inner">
 				<div id="content-text-inner">
 					<?php
-						if (isset($_GET['type']))
-						{
-							require_once('modules/file.php');
+						require_once('modules/file.php');
 
-							if (!isset($_GET['display']) && !isset($_GET['download']) && !isset($_GET['delete'])) {
-								switch($_GET['type'])
-								{
-									case "battleye":
-										$files = glob($patharma."\\@dayzcc_config\\".$serverinstance."\\BattlEye\\*.log");
-										if (count($files) > 0) {
-											echo '<table width="600px">';
-											foreach ($files as $filepath) {
-												$filename = substr($filepath, strrpos($filepath, "\\") + 1);
-												
-												if (file_exists($filepath)) {
-													echo '<tr><td width="30%"><a href="index.php?view=log&type=battleye&display='.$filename.'">'.$filename.'</a></td>
-														<td width="10%"><center><strong><a href="index.php?view=log&type=battleye&display='.$filename.'">View</a></strong></center></td>
-														<td width="10%"><center><strong><a href="index.php?view=log&type=battleye&download='.$filename.'">Download</a></strong></center></td>
-														<td width="10%"><center><strong><a href="index.php?view=log&type=battleye&delete='.$filename.'">Delete</a></strong></center></td>
-														<td width="15%">'.(formatBytes(filesize($filepath))).'</td>
-														<td width="25%">'.date("Y-M-d H:i:s", filemtime($filepath)).'</td></tr>';
-												}
+						if (!isset($_GET['display']) && !isset($_GET['download']) && !isset($_GET['delete'])) {
+							switch ($type)
+							{
+								case "battleye":
+									$files = glob($patharma."\\@dayzcc_config\\".$serverinstance."\\BattlEye\\*.log");
+									if (count($files) > 0) {
+										echo '<table width="600px">';
+										foreach ($files as $filepath) {
+											$filename = substr($filepath, strrpos($filepath, "\\") + 1);
+											
+											if (file_exists($filepath)) {
+												echo '<tr><td width="30%"><a href="index.php?view=log&type=battleye&display='.$filename.'">'.$filename.'</a></td>
+													<td width="10%"><center><strong><a href="index.php?view=log&type=battleye&display='.$filename.'">View</a></strong></center></td>
+													<td width="10%"><center><strong><a href="index.php?view=log&type=battleye&download='.$filename.'">Download</a></strong></center></td>
+													<td width="10%"><center><strong><a href="index.php?view=log&type=battleye&delete='.$filename.'">Delete</a></strong></center></td>
+													<td width="15%">'.(formatBytes(filesize($filepath))).'</td>
+													<td width="25%">'.date("Y-M-d H:i:s", filemtime($filepath)).'</td></tr>';
 											}
-											echo "</table>";
-										} else {
-											echo '<div id="page-heading"><h2>No BattlEye log files found.</h2></div>';
 										}
-										break;
-									case "server":
-										$files = glob($patharma."\\@dayzcc_config\\".$serverinstance."\\*.{LOG,log,RPT,rpt}", GLOB_BRACE);
-										if (count($files) > 0) {
-											echo '<table width="600px">';
-											foreach ($files as $filepath) {
-												$filename = substr($filepath, strrpos($filepath, "\\") + 1);
-												
-												if (file_exists($filepath)) {
-													echo '<tr><td width="30%"><a href="index.php?view=log&type=server&display='.$filename.'">'.$filename.'</a></td>
-														<td width="10%"><center><strong><a href="index.php?view=log&type=server&display='.$filename.'">View</a></strong></center></td>
-														<td width="10%"><center><strong><a href="index.php?view=log&type=server&download='.$filename.'">Download</a></strong></center></td>
-														<td width="10%"><center><strong><a href="index.php?view=log&type=server&delete='.$filename.'">Delete</a></strong></center></td>
-														<td width="15%">'.(formatBytes(filesize($filepath))).'</td>
-														<td width="25%">'.date("Y-M-d H:i:s", filemtime($filepath)).'</td></tr>';
-												}
+										echo "</table>";
+									} else {
+										echo '<div id="page-heading"><h2>No BattlEye log files found.</h2></div>';
+									}
+									break;
+								case "server":
+									$files = glob($patharma."\\@dayzcc_config\\".$serverinstance."\\*.{LOG,log,RPT,rpt}", GLOB_BRACE);
+									if (count($files) > 0) {
+										echo '<table width="600px">';
+										foreach ($files as $filepath) {
+											$filename = substr($filepath, strrpos($filepath, "\\") + 1);
+											
+											if (file_exists($filepath)) {
+												echo '<tr><td width="30%"><a href="index.php?view=log&type=server&display='.$filename.'">'.$filename.'</a></td>
+													<td width="10%"><center><strong><a href="index.php?view=log&type=server&display='.$filename.'">View</a></strong></center></td>
+													<td width="10%"><center><strong><a href="index.php?view=log&type=server&download='.$filename.'">Download</a></strong></center></td>
+													<td width="10%"><center><strong><a href="index.php?view=log&type=server&delete='.$filename.'">Delete</a></strong></center></td>
+													<td width="15%">'.(formatBytes(filesize($filepath))).'</td>
+													<td width="25%">'.date("Y-M-d H:i:s", filemtime($filepath)).'</td></tr>';
 											}
-											echo "</table>";
-										} else {
-											echo '<div id="page-heading"><h2>No server log files found.</h2></div>';
 										}
-										break;
-								}
-							} else {
-								if (isset($_GET['display'])) {
-									$file = $_GET['display'];
-									if ($file != "") {
-										$path_parts = pathinfo($file);
-										$file = $path_parts['basename'];
+										echo "</table>";
+									} else {
+										echo '<div id="page-heading"><h2>No server log files found.</h2></div>';
+									}
+									break;
+							}
+						} else {
+							if (isset($_GET['display'])) {
+								$file = $_GET['display'];
+								if ($file != "") {
+									$path_parts = pathinfo($file);
+									$file = $path_parts['basename'];
+									
+									if (substr(strtolower($file), -strlen(".log")) === ".log" || substr(strtolower($file), -strlen(".log")) === ".rpt") {
 										$filepath = $patharma."\\@dayzcc_config\\".$serverinstance."\\".($_GET['type'] == "battleye" ? "BattlEye\\" : "").$file;
 										
 										if (file_exists($filepath)) {
@@ -104,36 +107,35 @@ if (isset($_SESSION['user_id']) and (strpos($_SESSION['user_permissions'], "mana
 									} else {
 										echo '<div id="page-heading"><h2>Invalid file specified.<h2></div>';
 									}
-								} else if (isset($_GET['download'])) {
-									$file = $_GET['download'];
-									if ($file != "") {
-										$path_parts = pathinfo($file);
-										$file = $path_parts['basename'];
-										$filepath = str_replace("\\", "__dir__", $patharma."\\@dayzcc_config\\".$serverinstance."\\".($_GET['type'] == "battleye" ? "BattlEye\\" : ""));
-										echo '<script type="text/javascript">window.open("modules/lib/download.php?path='.$filepath.'&file='.$file.'"); window.location = "index.php?view=log&type='.$_GET['type'].'";</script>';
-									} else {
-										echo '<div id="page-heading"><h2>Invalid file specified.<h2></div>';
-									}
-								} else if (isset($_GET['delete'])) {
-									$file = $_GET['delete'];
-									if ($file != "") {
-										$path_parts = pathinfo($file);
-										$file = $path_parts['basename'];
-										$filepath = $patharma."\\@dayzcc_config\\".$serverinstance."\\".($_GET['type'] == "battleye" ? "BattlEye\\" : "").$file;
+								} else {
+									echo '<div id="page-heading"><h2>Invalid file specified.<h2></div>';
+								}
+							} else if (isset($_GET['download'])) {
+								$file = $_GET['download'];
+								if ($file != "") {
+									echo '<script type="text/javascript">window.open("modules/lib/download.php?file='.$file.($_GET['type'] == "battleye" ? "&battleye" : "").'"); window.location = "index.php?view=log&type='.$_GET['type'].'";</script>';
+								} else {
+									echo '<div id="page-heading"><h2>Invalid file specified.<h2></div>';
+								}
+							} else if (isset($_GET['delete'])) {
+								$file = $_GET['delete'];
+								if ($file != "") {
+									$path_parts = pathinfo($file);
+									$file = $path_parts['basename'];
+									$filepath = $patharma."\\@dayzcc_config\\".$serverinstance."\\".($_GET['type'] == "battleye" ? "BattlEye\\" : "").$file;
 
-										if (file_exists($filepath) && is_file($filepath)) {
-											if (isset($_GET['confirm'])) {
-												unlink($filepath);
-												echo '<script type="text/javascript">window.location = "index.php?view=log&type='.$_GET['type'].'";</script>';
-											} else {
-												echo '<script type="text/javascript">if (confirm("Do you really want to delete \"'.$_GET['delete'].'\"?")) { window.location = "index.php?view=log&type='.$_GET['type'].'&delete='.$_GET['delete'].'&confirm"; }</script>';
-											}
+									if (file_exists($filepath) && is_file($filepath)) {
+										if (isset($_GET['confirm'])) {
+											unlink($filepath);
+											echo '<script type="text/javascript">window.location = "index.php?view=log&type='.$_GET['type'].'";</script>';
 										} else {
-											echo '<div id="page-heading"><h2>File not found: '.$filepath.'<h2></div>';
+											echo '<script type="text/javascript">if (confirm("Do you really want to delete \"'.$_GET['delete'].'\"?")) { window.location = "index.php?view=log&type='.$_GET['type'].'&delete='.$_GET['delete'].'&confirm"; } else { window.location = "index.php?view=log&type='.$_GET['type'].'"; }</script>';
 										}
 									} else {
-										echo '<div id="page-heading"><h2>Invalid file specified.<h2></div>';
+										echo '<div id="page-heading"><h2>File not found: '.$filepath.'<h2></div>';
 									}
+								} else {
+									echo '<div id="page-heading"><h2>Invalid file specified.<h2></div>';
 								}
 							}
 						}
