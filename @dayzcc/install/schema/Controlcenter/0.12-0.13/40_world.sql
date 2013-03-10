c3062865d6eb51ddf76790889ea7c0292b9c8abb
Crosire <crosiregaming@googlemail.com>
2013-03-10 11:32:27 +0100

    Added support for most new maps

---
+++ @dayzcc/install/schema/Controlcenter/0.12-0.13/40_world.sql
@@ -0,0 +1,3 @@
+update world set folder = '@dayz_namalsk_@dayz;@dayz_namalsk' where name = 'namalsk';
+update world set folder = '@dayzthirsk' where name = 'thirsk';
+update world set folder = '@dayz_oring' where name = 'oring';
